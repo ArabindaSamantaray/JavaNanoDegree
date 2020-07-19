c328336f74cca5273f986e9b50cf48c4a8aaa12c
ArabindaSamantaray <40869951+ArabindaSamantaray@users.noreply.github.com>
2020-07-19 14:31:41 -0700

    Learning Spring bean initailaisation and dependency injection

--- SpringBasics/src/main/java/com/example/arabinda/SpringBasics/SpringBasicsApplication.java
+++ SpringBasics/src/main/java/com/example/arabinda/SpringBasics/SpringBasicsApplication.java
@@ -2,12 +2,32 @@ package com.example.arabinda.SpringBasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBasicsApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(SpringBasicsApplication.class, args);
 	}
+
+	@Bean
+    public String message(){
+        System.out.println("Message bean was created");
+        return "Hello, Spring!";
+    }
+
+    @Bean
+    public String upperCase(MessageService messageService){
+        System.out.println("Uppercase message bean was created");
+        return messageService.upperCase();
+    }
+
+    @Bean
+    public String lowerCase(MessageService messageService){
+        System.out.println("Lowercase message bean was created");
+        return messageService.lowerCase();
+    }
+
 
 }
